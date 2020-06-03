0a0e79e1cf19d8a2af436dbfc1dacf22e260e080
Alex-kku <leha.kushpelev@mai.ru>
2020-06-03 21:58:20 +0300

    Homework done

---
+++ hello_world_application/_build/CMakeFiles/Hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello.dir/home/baha/Alex-kku/workspace/projects/homework03/tmp/formatter_ex_lib/formatter_ex.cpp.o"
+  "CMakeFiles/Hello.dir/home/baha/Alex-kku/workspace/projects/homework03/tmp/formatter_lib/formatter.cpp.o"
+  "libHello.pdb"
+  "libHello.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
